65d303966f781f9870ab524d0d29a224947d51a7
Rob Olson <rholson1@wisc.edu>
2023-01-06 08:20:20 -0600

    revert to running at root path

--- application.py
+++ application.py
@@ -4,10 +4,7 @@ import dash_bootstrap_components as dbc
 from app_main import layout
 from app_main.callbacks import register_callbacks
 
-app = Dash(__name__, external_stylesheets=[dbc.icons.FONT_AWESOME],
-           routes_pathname_prefix='/',
-           requests_pathname_prefix='/mangem_aws/')
-#           url_base_pathname='/mangem_aws/')
+app = Dash(__name__, external_stylesheets=[dbc.icons.FONT_AWESOME])
 app.title = 'MANGEM'
 application = app.server
 
